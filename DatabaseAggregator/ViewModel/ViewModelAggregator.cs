e7bf547947c8ddb5de76081e04d24b439d52307c
Никита Галямин <galyamin03@mail.ru>
2024-09-18 22:21:58 +0300

    1.2

--- DatabaseAggregator/ViewModel/ViewModelAggregator.cs
+++ DatabaseAggregator/ViewModel/ViewModelAggregator.cs
@@ -1,7 +1,13 @@
 ﻿using DatabaseAggregator.Core;
 using DatabaseAggregator.Model;
 using DatabaseAggregator.View;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Windows;
+using System.Windows.Shapes;
 
 namespace DatabaseAggregator.ViewModel
 {
@@ -15,8 +21,9 @@ namespace DatabaseAggregator.ViewModel
 
         public RelayCommand HelpViewCommand { get; set; }
         public RelayCommand SettingViewCommand { get; set; }
-        public RelayCommand BaseViewCommand { get; set; }
+        public RelayCommand CrateViewCommand { get; set; }
         public RelayCommand SettingViewButtonCommand { get; set; }
+        public RelayCommand SaveViewCommand { get; set; }
 
         private string urlNvd;
         public string UrlNvd
@@ -83,31 +90,122 @@ namespace DatabaseAggregator.ViewModel
             }
         }
 
+        private string helpText;
+        public string HelpText
+        {
+            get { return helpText; }
+            set { helpText = value; }
+        }
+
+        private ProgramConfiguration progConfig;
+        public ProgramConfiguration ProgConfig
+        {
+            get { return progConfig; }
+            set { progConfig = value; }
+        }
+
+        private ObservableCollection<Database> database;
+        public ObservableCollection<Database> Database
+        {
+            get { return database; }
+            set { database = value; }
+        }
+
+        private readonly string pathToResources = System.IO.Path.Combine(Environment.CurrentDirectory, "Resource");
+
         public ViewModelAggregator()
         {
-/*         BV = new();
+            BV = new();
             SV = new();
             HV = new();
             model = new();
 
-            HelpViewCommand = new RelayCommand(o =>
-              {
-                  CurrentView = HV;
-              });
+            if (!Directory.Exists(pathToResources))
+                Directory.CreateDirectory(pathToResources);
 
-            SettingViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = SV;
+            ImportFile(false);
+
+            SaveViewCommand = new RelayCommand(o =>
+            {     
+                SaveFile(Database);
             });
-            BaseViewCommand = new RelayCommand(o =>
+
+            CrateViewCommand = new RelayCommand(o =>
             {
                 CurrentView = BV;
+                CreatFile();
             });
-            SettingViewButtonCommand = new RelayCommand(o =>
+
+            /*         
+
+                        HelpViewCommand = new RelayCommand(o =>
+                          {
+                              CurrentView = HV;
+                          });
+
+                        SettingViewCommand = new RelayCommand(o =>
+                        {
+                            CurrentView = SV;
+                        });
+                        SettingViewButtonCommand = new RelayCommand(o =>
+                        {
+                            ProgramConfiguration progConfig = new(UrlFstek, UrlNvd, ApiKeyNvd, UrlJvn);
+                            model.SaveFile(progConfig);
+                        });*/
+        }
+
+        public void ImportFile(bool selectionWindow) 
+        {
+            if(selectionWindow)
             {
-                ProgramConfiguration progConfig = new(UrlFstek, UrlNvd, ApiKeyNvd, UrlJvn);
-                model.SaveFile(progConfig);
-            });*/
+                OpenFileDialog openFileDialog = new() { Filter = "Json files (*.json)|*.json" };
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    var filePath = openFileDialog.FileName;
+                    string jsonString = File.ReadAllText(filePath);
+                    Database = JsonSerializer.Deserialize<ObservableCollection<Database>>(jsonString);
+                }
+            }
+            else
+            {
+                var filePath = System.IO.Path.Combine(pathToResources, "programConfiguration.json");
+                if (!System.IO.Path.Exists(filePath))
+                {
+                    MessageBox.Show("Файл конфигурации отсутствует, заполните его вручную");
+                }
+                else
+                {
+                    string jsonString = File.ReadAllText(filePath);
+                    ProgConfig = JsonSerializer.Deserialize<ProgramConfiguration>(jsonString);
+                }               
+            }        
+        }
+
+        public void CreatFile()
+        {
+            if(ProgConfig is null)
+            {
+                MessageBox.Show("Отсутсвует файл конфигурации, создайте его вручную", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                //Database = model.CreatDatabase();
+            }           
+        }
+        public void SaveFile(object objectSerializations)
+        {
+            SaveFileDialog saveFileDialog = new() { Filter = "Json files (*.json)|*.json|All files (*.*)|*.*" };
+            if (saveFileDialog.ShowDialog() is true)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                };
+                var json = JsonSerializer.Serialize(objectSerializations, options);
+                File.WriteAllText(saveFileDialog.FileName, json);
+            }
         }
     }
 }
